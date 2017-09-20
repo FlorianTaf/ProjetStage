4e872222783b58bdc234defaef7e000a09e73e80
Florian <florian.taffaneau@laposte.net>
2017-09-20 10:24:41 +0200

    faischier

---
+++ src/FT/ProjetStageBundle/Controller/RegistrationController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Florian
+ * Date: 20/09/2017
+ * Time: 10:24
+ */
+
+namespace FT\ProjetStageBundle\Controller;
+
+
+class RegistrationController
+{
+
+}
